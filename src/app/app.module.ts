380394bbb7d6347cfa2c3853243b0eecba5a216b
udith-wiratunga <udith.wiratunga@gmail.com>
2019-04-17 11:20:46 +0530

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { StickyNoteComponent } from './sticky/sticky.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, StickyNoteComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/sticky/sticky.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sticky',
+  templateUrl: './sticky.component.html',
+  styleUrls: ['./sticky.component.css']
+})
+export class StickyNoteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
