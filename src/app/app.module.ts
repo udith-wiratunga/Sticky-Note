784edac6fe3a688e90b5259c60d840cfe6ddabf9
udith-wiratunga <udith.wiratunga@gmail.com>
2019-04-17 14:43:18 +0530

    Add new component 4 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { StickyNoteComponent } from './sticky/sticky.component';
+import { StickyListComponent } from './sticky-list/sticky-list.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, StickyNoteComponent ],
+  declarations: [ AppComponent, HelloComponent, StickyNoteComponent, StickyListComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/sticky-list/sticky-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sticky-list',
+  templateUrl: './sticky-list.component.html',
+  styleUrls: ['./sticky-list.component.css']
+})
+export class StickyListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
