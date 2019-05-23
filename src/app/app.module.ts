81c1551956b87923f205d531287696caa840d06c
udith-wiratunga <udith.wiratunga@gmail.com>
2019-05-23 11:04:29 +0530

    3 files modified, nav bar component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { StickyNoteComponent } from './sticky/sticky.component';
 import { StickyListComponent } from './sticky-list/sticky-list.component';
 import { HttpClientModule }    from '@angular/common/http';
 import { NotesService } from './notes.service';
+import { NavComponent } from './components/nav/nav/nav.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,HttpClientModule ],
-  declarations: [ AppComponent,  StickyNoteComponent, StickyListComponent ],
+  declarations: [ AppComponent,  StickyNoteComponent, StickyListComponent, NavComponent ],
   bootstrap:    [ AppComponent ],
   providers: [NotesService]
 })
