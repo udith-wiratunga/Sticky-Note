3ea415d257feb296f95156adc0cff11edadc0559
udith-wiratunga <udith.wiratunga@gmail.com>
2019-05-25 21:20:55 +0530

    move search to nav bar and add service links

--- src/app/components/nav/nav/nav.component.ts
+++ src/app/components/nav/nav/nav.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-//import { NotesService } from '../notes.service';
-//import { Note } from './note';
+import { NotesService } from '../../../notes.service';
+import { Note } from '../../../note';
 
 @Component({
   selector: 'app-nav',
@@ -9,16 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
   title:string;
-  //Notes:Note[];
-  //constructor(private noteService:NotesService) { }
-constructor() { }
+  Notes:Note[];
+  constructor(private noteService:NotesService) { }
+
   ngOnInit() {
     this.title="Hello Angular"
   }
 
   search(title:string){
-    console.log("Component");
-  //  this.noteService.search(title).subscribe(Notes=>this.Notes=Notes);
+    console.log(title);
+    this.noteService.search(title).subscribe(Notes=>this.Notes=Notes);
   }
 
 }
