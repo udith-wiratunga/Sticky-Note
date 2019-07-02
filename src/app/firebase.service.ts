6791f99eec6aca7eb1d717ca5c510023bf8aec8c
udith-wiratunga <udith.wiratunga@gmail.com>
2019-07-02 05:44:38 -0500

    2 files modified

--- src/app/firebase.service.ts
+++ src/app/firebase.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
-
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 
 @Injectable()
 export class FirebaseService {
-
+///https://www.youtube.com/watch?v=cwqeyOFcaoA&list=PLillGF-RfqbaISD5mxDCIjsSYk4jbiXi4&index=2
   //firebase.initializeApp(config);
+  noteCollection:AngularFirestoreCollection<note>;
+  constructor(public afs:AngularFirestore) { 
 
-  constructor() { }
+  }
 
 }
