a5f42dcbf1f7e437c1960013a2fa736931da2c0f
udith-wiratunga <udith.wiratunga@gmail.com>
2019-07-02 04:23:52 -0500

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,10 +16,24 @@ import { MembersComponent } from './components/user/members/members.component';
 import { SignupComponent } from './components/user/signup/signup.component';
 import { StickyAlertComponent } from './components/common/sticky-alert/sticky-alert.component';
 import { ProgressComponent } from './components/common/progress/progress.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
 import { FirebaseService } from './firebase.service';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAWlKJBHRGvHn9y5CfOOBFyxXWEad4jyOg",
+  authDomain: "stickynotedb.firebaseapp.com",
+  databaseURL: "https://stickynotedb.firebaseio.com",
+  projectId: "stickynotedb",
+  storageBucket: "stickynotedb.appspot.com",
+  messagingSenderId: "301539775082",
+  appId: "1:301539775082:web:20086d3ef23d52d3"
+};
+
 @NgModule({
-  imports:      [ BrowserModule, BrowserAnimationsModule, FormsModule,HttpClientModule ],
+  imports:      [ BrowserModule, BrowserAnimationsModule, FormsModule,HttpClientModule,   AngularFireModule.initializeApp(firebaseConfig),
+   AngularFireDatabaseModule ],
   declarations: [ AppComponent,  StickyNoteComponent, StickyListComponent, NavComponent, SearchComponent, LoginComponent, EmailComponent, MembersComponent, SignupComponent, StickyAlertComponent, ProgressComponent ],
   bootstrap:    [ AppComponent ],
   providers: [NotesService, FirebaseService]
