dd1c7e98ca2b488b69fb0edce5313b8a17d4dd5b
udith-wiratunga <udith.wiratunga@gmail.com>
2019-07-03 04:50:52 -0500

    1 file modified

--- src/app/firebase.service.ts
+++ src/app/firebase.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { Note } from './note';
-import { Observable } from 'rxjs';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class FirebaseService {
