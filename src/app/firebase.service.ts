932e353e9f1ae7fb3f3b7b3194ca70ff4fc2e7c2
udith-wiratunga <udith.wiratunga@gmail.com>
2019-07-03 03:51:20 -0500

    Add Note file modified

--- src/app/firebase.service.ts
+++ src/app/firebase.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Note } from './note';
 
 @Injectable()
 export class FirebaseService {
 ///https://www.youtube.com/watch?v=cwqeyOFcaoA&list=PLillGF-RfqbaISD5mxDCIjsSYk4jbiXi4&index=2
   //firebase.initializeApp(config);
-  noteCollection:AngularFirestoreCollection<note>;
+  noteCollection:AngularFirestoreCollection<Note>;
   constructor(public afs:AngularFirestore) { 
 
   }
