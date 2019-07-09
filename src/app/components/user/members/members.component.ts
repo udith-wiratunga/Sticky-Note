bd8d2e672dcccdf672f9695aed328fb88da401dc
udith-wiratunga <udith.wiratunga@gmail.com>
2019-07-09 01:39:10 -0500

    1 file modified

--- src/app/components/user/members/members.component.ts
+++ src/app/components/user/members/members.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFire, AuthProviders, AuthMethods } from 'angularfire2';
+import { AngularFireAuth } from "angularfire2/auth";
+import * as firebase from 'firebase/app';
 import { Router } from '@angular/router';
 import { moveIn, fallIn, moveInLeft } from '../../../router.animations';
 
@@ -14,17 +15,16 @@ export class MembersComponent implements OnInit {
   name: any;
   state: string = '';
 
-  constructor(public af: AngularFire,private router: Router) {
-    this.af.auth.subscribe(auth => {
-      if(auth) {
-        this.name = auth;
-      }
-    });
+  constructor(public af: AngularFireAuth,private router: Router) {
+    this.af.authState.subscribe(auth => { 
+        if(auth) {
+          this.router.navigateByUrl('/members');
+        }
+      });
   }
 
   logout() {
-     this.af.auth.logout();
-     console.log('logged out');
+     this.af.auth.signOut();
      this.router.navigateByUrl('/login');
   }
 
