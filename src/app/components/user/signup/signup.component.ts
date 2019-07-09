09f0b6662d1f3dff675b4aa3835f63770947b4c9
udith-wiratunga <udith.wiratunga@gmail.com>
2019-07-08 22:31:48 -0500

    1 file modified

--- src/app/components/user/signup/signup.component.ts
+++ src/app/components/user/signup/signup.component.ts
@@ -7,9 +7,9 @@ import { moveIn, fallIn } from '../../../router.animations';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']/*,
+  styleUrls: ['./signup.component.css'],
   animations: [moveIn(), fallIn()],
-  host: {'[@moveIn]': ''}*/
+  host: {'[@moveIn]': ''}
 })
 export class SignupComponent implements OnInit {
   state: string = '';
