b9e879471d085e0dc09ec9e17d22a052bcce707d
udith-wiratunga <udith.wiratunga@gmail.com>
2019-07-11 05:05:24 -0500

    1 file modified

--- src/app/components/nav/nav/nav.component.ts
+++ src/app/components/nav/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
 import { AngularFireAuth } from "angularfire2/auth";
 import * as firebase from 'firebase/app';
 import { Router } from '@angular/router';
