5fffa22da07b95009f40b86d398cf094a9c62bcc
udith-wiratunga <udith.wiratunga@gmail.com>
2019-07-22 05:27:35 -0500

    1 file modified

--- src/app/firebase.service.ts
+++ src/app/firebase.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import 'rxjs/add/operator/map';
 import { Observable} from 'rxjs/Observable';
+import { AngularFireAuth } from "angularfire2/auth";
 //import { Observable, of } from 'rxjs';
 import { Note } from './note';
 
@@ -13,26 +14,32 @@ export class FirebaseService {
   noteDoc:AngularFirestoreDocument<Note>;
   tempNotes:Observable<Note[]>;
   
-  constructor(public afs:AngularFirestore) {
+  constructor(public afs:AngularFirestore,public af:AngularFireAuth) {
     //https://dev.to/crazedvic/query--update-firestore-documents-in-angular-7-5fhg
     //this.noteCollection = this.afs.collection<Note>('note',ref=>ref.where('email','==','udith@abc.com'));
-   
+      this.af.authState.subscribe(auth => { 
+        if(auth) {
+
+          this.noteCollection = this.afs.collection<Note>('note',ref=>ref.where('email','==',auth.email));
+          this.notes = this.noteCollection.snapshotChanges().map(changes => {
+            return changes.map( action => {
+              const data = action.payload.doc.data() as Note;
+              data.id = action.payload.doc.id;
+              //console.log(data);
+              return data;
+            })
+          });
+        }
+      });
+
+    
+    
 
   }
 
   
 
-  getNotes(UserEmail:string):Observable<Note[]>{
-    //console.log(this.notes);
-    this.noteCollection = this.afs.collection<Note>('note',ref=>ref.where('email','==',UserEmail));
-    this.notes = this.noteCollection.snapshotChanges().map(changes => {
-      return changes.map( action => {
-        const data = action.payload.doc.data() as Note;
-        data.id = action.payload.doc.id;
-        //console.log(data);
-        return data;
-      })
-    });
+  getNotes():Observable<Note[]>{
     return this.notes;
   }
 
